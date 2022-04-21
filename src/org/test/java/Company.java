af51b361c1db89eabb82207fb9d87ebf63477f55
Manojmj <n.manojmj1997@gmail.com>
2022-04-21 21:29:44 +0530

    message

---
+++ src/org/test/java/Company.java
@@ -0,0 +1,21 @@
+package org.test.java;
+
+import java.util.Scanner;
+
+public class Company {
+
+	public static void main(String[] args) {
+
+	Scanner s = new Scanner(System.in);
+	System.out.println("enter the number");
+	int a = s.nextInt();
+
+	if(a%2==0) {
+		System.out.println("even");
+		}
+	else {
+		System.out.println("odd");
+	}
+	}
+		
+	}
